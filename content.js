91b188d885928b814b9a02fcf49c151acd7606b7
bonny5151 <lkjelkjl@alkj.com>
2023-10-07 02:05:57 -0400

    a

--- content.js
+++ content.js
@@ -1,5 +1,19 @@
-setInterval(()=> {
+del()
+setTimeout(del, 500)
+setTimeout(del,1000)
+setTimeout(del,1500)
+setTimeout(del,2000)
+setInterval( del, 2000)
+
 //var xpathq = '//div[contains(@class,"articlestyles") or contains(@class,"opinionstyles")][.//*[text()[contains(.,"Daniel Kuhn")]]]'
+
+function del() {
+delvideo()
+delarticle()
+
+}
+
+function delarticle() {
 var xpathq = '//*[text()[contains(.,"Daniel Kuhn")]]/ancestor::div[contains(@class,"StaticCardWrapper") or contains(@class,"opinionstyle") or contains(@class,"articlestyle")][1]'
 
 function xp2(xp) {return document.evaluate(
@@ -8,12 +22,26 @@ function xp2(xp) {return document.evaluate(
   );  }
 
 var xresult = xp2(xpathq)
-for(var i = 0; i < xresult.snapshotLength; i++) { var aa = xresult.snapshotItem(i); aa.remove();}
+var url = []
+for(var i = 0; i < xresult.snapshotLength; i++) { 
 
+  var aa = xresult.snapshotItem(i); 
+  
+   var a1 = aa.querySelector("a:not([href^='/author'])")
+   if(a1) { url.push( a1.getAttribute("href") ); }
+  aa.remove();
+
+}
+for(var i =0; i < url.length; i++) { 
+  var a1 = document.querySelector("div.live-wire a[href='" + url[i] + "'");
+  if (a1) { a1.parentElement.parentElement.parentElement.remove()}
+ }
+}
+function delvideo() {
 var images = document.querySelectorAll("div.jwplayer");
 
   for (var i = 0, l = images.length; i < l; i++) {
   	    try { images[i].remove(); } catch (e) {}
   }
-},1000)
+}
 
